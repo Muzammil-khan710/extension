f9fd2736375c06ceabfaca2a5749417abab9edfd
Muzammil-khan710 <khanmuzammil549@gmail.com>
2023-01-11 15:49:43 +0530

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import './App.css'
+
 const App = () => {
+
   return (
-    <div>
-      Hello chrome extension
+    <div className="login-form">
+      <form className='form'>
+        <label htmlFor="email" className='label'>
+          Email
+          <input type='text' id="email" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type='password' id="password" className='label'/>
+          </label>
+          <input type="submit" value="Submit" className='submit-btn s-center'   />
+      </form>
+      <div>Or</div>
+
+      <div>
+        <button className='submit-btn'>Continue with google</button>
+      </div>
     </div>
   )
 }
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import { Configuration, OpenAIApi } from "openai";
+// const configuration = new Configuration({
+//     organization: "org-NKtPQJZvG3jJWCicIeKBesk7",
+//     apiKey: 'psk-t9oN5IAxkuVnTe3h4QOIT3BlbkFJLcNkbP3yOCTZXsXeLNG3',
+// });
+// const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+// console.log(response)
+
+const express = require('express')
