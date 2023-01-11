fbced751ad846775779c7310dc1f56ad07297e27
Muzammil-khan710 <khanmuzammil549@gmail.com>
2023-01-11 16:23:57 +0530

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,7 @@ import './App.css'
 const App = () => {
 
   return (
-    <div className="login-form">
-      <form className='form'>
-        <label htmlFor="email" className='label'>
-          Email
-          <input type='text' id="email" />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input type='password' id="password" className='label'/>
-          </label>
-          <input type="submit" value="Submit" className='submit-btn s-center'   />
-      </form>
-      <div>Or</div>
-
-      <div>
-        <button className='submit-btn'>Continue with google</button>
-      </div>
+    <div>  
     </div>
   )
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,24 @@
+const Login = () => {
+  return (
+    <div className="login-form">
+    <form className='form'>
+      <label htmlFor="email" className='label'>
+        Email
+        <input type='text' id="email" />
+      </label>
+      <label htmlFor="password">
+        Password
+        <input type='password' id="password" className='label'/>
+        </label>
+        <input type="submit" value="Submit" className='submit-btn s-center'   />
+    </form>
+    <div>Or</div>
+
+    <div>
+      <button className='submit-btn'>Continue with google</button>
+    </div>
+  </div>
+  )
+}
+
+export default Login
